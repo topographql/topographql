a7e0e2ea407e10a20e65cc1473d1ce2d5f2156f3
Taylor Quartararo <t_q@Taylors-MacBook-Air.local>
2020-04-07 16:54:03 -0400

    added sass to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ],
   },
   resolve: {
