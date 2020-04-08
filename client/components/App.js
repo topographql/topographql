7455db453ecfe9a289eb14e4ac0220d267f25db5
Taylor Quartararo <t_q@Taylors-MacBook-Air.local>
2020-04-08 18:45:05 -0400

    made .js files compatable with ts

---
+++ client/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ToolBarContainer from "./ToolBarContainer"
+import VisualizerContainer from "./VisualizerContainer"
+
+class App extends React.Component{
+    constructor(){
+      super()
+    }
+
+    render(){
+      return(
+        <div> 
+          <ToolBarContainer /> 
+          <VisualizerContainer /> 
+        </div>
+      )
+    }
+}
+
+export default App
+
+
+
---
+++ client/components/ToolBarContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToolBarContainer() {
+  return (
+      <div>
+         ToolBar Container
+      </div>
+  )
+}
+
+export default ToolBarContainer
