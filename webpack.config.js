a43e42c3e059e733dc797101fe8ea2d66bb46480
Daniel Chell <dgchell@gmail.com>
2020-04-09 11:28:49 -0400

    dan original work

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    mode: process.env.NODE_ENV, 
-    entry: {
-      index: ['./client/index.js'],
-    },
-    output: { 
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'build'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          loader: 'babel-loader',
-          exclude: path.resolve(__dirname, 'node_modules'),
-          query: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+  mode: process.env.NODE_ENV,
+  entry: {
+    index: ['./client/index.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules'),
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
-      ]
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    publicPath: '/build/',
+    contentBase: path.join(__dirname, './client'), // path from which static file should be served. if not specified, static files will not be served.
+    proxy: {
+      '/users': 'http://localhost:3000',
     },
-    devServer: {
-      historyApiFallback: true,
-      publicPath: '/build/',
-      contentBase: path.join(__dirname, './client'), // path from which static file should be served. if not specified, static files will not be served.
-      proxy: {
-        '/users':'http://localhost:3000',
-      }
-    },
-  }
+  },
+};
