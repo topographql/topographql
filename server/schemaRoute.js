885df4345d620e620acda5359bb96ab8174d86a0
Meisze Yau <meiszey@gmail.com>
2020-04-11 14:15:10 -0400

    Deleted unnecessary files

--- server/schemaRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-import schemaController from './schemaController';
-
-router.post('/getschema', schemaController.convertSchema, (req, res, next) => {
-
-} );
