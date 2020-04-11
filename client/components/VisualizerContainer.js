a515c147c5ca7ab01675ee77c294e87b778732b0
Taylor Quartararo <quartararo.t@gmail.com>
2020-04-11 13:52:41 -0400

    renders from state

--- client/components/VisualizerContainer.js
+++ client/components/VisualizerContainer.js
@@ -8,207 +8,101 @@ class VisualizerContainer extends Component {
   }
 
   drawChart() {
-    const hardData = {
-      links: [
-        {
-          source: 'info',
-          target: 'Info',
-        },
-        {
-          source: 'Query',
-          target: 'info',
-        },
-        {
-          source: 'teams',
-          target: 'Team',
-        },
-        {
-          source: 'Query',
-          target: 'teams',
-        },
-        {
-          source: 'players',
-          target: 'Player',
-        },
-        {
-          source: 'Query',
-          target: 'players',
-        },
-        {
-          source: 'games',
-          target: 'Fixture',
-        },
-        {
-          source: 'Query',
-          target: 'games',
-        },
-        {
-          source: 'fixtures',
-          target: 'Fixture',
-        },
-        {
-          source: 'Query',
-          target: 'fixtures',
-        },
-        {
-          source: 'Info',
-          target: 'caption',
-        },
-        {
-          source: 'Info',
-          target: 'league',
-        },
-        {
-          source: 'Info',
-          target: 'year',
-        },
-        {
-          source: 'Info',
-          target: 'currentMatchday',
-        },
-        {
-          source: 'Info',
-          target: 'numberOfMatchdays',
-        },
-        {
-          source: 'Info',
-          target: 'numberOfTeams',
-        },
-        {
-          source: 'Info',
-          target: 'numberOfGames',
-        },
-        {
-          source: 'Info',
-          target: 'lastUpdated',
-        },
-        {
-          source: 'Team',
-          target: 'id',
-        },
-        {
-          source: 'Team',
-          target: 'name',
-        },
-        {
-          source: 'Team',
-          target: 'code',
-        },
-        {
-          source: 'Team',
-          target: 'shortName',
-        },
-        {
-          source: 'Team',
-          target: 'squadMarketValue',
-        },
-        {
-          source: 'Team',
-          target: 'flag',
-        },
-      ],
-    };
+    const nodes = {};
+    const { links } = this.props.d3introspectdata; // add object passed from state here
 
-    // d3.json('../public/d3schemaWC.json', (data) => {
-    //   console.log(data);
-      console.log(hardData)
-      const nodes = {};
-      const { links } = hardData;
+    links.forEach((link) => {
+      link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
+      link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
+    });
 
-      links.forEach((link) => {
-        link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
-        link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
-      });
+    const w = 960;
+    const h = 500;
 
-      const w = 960;
-      const h = 500;
+    const force = d3.layout
+      .force()
+      .nodes(d3.values(nodes))
+      .links(links)
+      .size([w, h])
+      .linkDistance(60)
+      .charge(-300)
+      .on('tick', tick)
+      .start();
 
-      const force = d3.layout
-        .force()
-        .nodes(d3.values(nodes))
-        .links(links)
-        .size([w, h])
-        .linkDistance(60)
-        .charge(-300)
-        .on('tick', tick)
-        .start();
+    const svg = d3
+      .select('#myD3')
+      .append('svg:svg')
+      .attr('width', w)
+      .attr('height', h);
 
-      const svg = d3
-        .select('#myD3')
-        .append('svg:svg')
-        .attr('width', w)
-        .attr('height', h);
+    // Per-type markers, as they don't inherit styles.
+    svg
+      .append('svg:defs')
+      .selectAll('marker')
+      .data(['suit', 'licensing', 'resolved', 'child'])
+      .enter()
+      .append('svg:marker')
+      .attr('id', String)
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 15)
+      .attr('refY', -1.5)
+      .attr('markerWidth', 6)
+      .attr('markerHeight', 6)
+      .attr('orient', 'auto')
+      .append('svg:path')
+      .attr('d', 'M0,-5L10,0L0,5');
 
-      // Per-type markers, as they don't inherit styles.
-      svg
-        .append('svg:defs')
-        .selectAll('marker')
-        .data(['suit', 'licensing', 'resolved', 'child'])
-        .enter()
-        .append('svg:marker')
-        .attr('id', String)
-        .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 15)
-        .attr('refY', -1.5)
-        .attr('markerWidth', 6)
-        .attr('markerHeight', 6)
-        .attr('orient', 'auto')
-        .append('svg:path')
-        .attr('d', 'M0,-5L10,0L0,5');
+    const path = svg
+      .append('svg:g')
+      .selectAll('path')
+      .data(force.links())
+      .enter()
+      .append('svg:path')
+      .attr('class', (d) => 'link child')
+      .attr('marker-end', (d) => 'url(#child)');
 
-      const path = svg
-        .append('svg:g')
-        .selectAll('path')
-        .data(force.links())
-        .enter()
-        .append('svg:path')
-        .attr('class', (d) => 'link child')
-        .attr('marker-end', (d) => 'url(#child)');
-
-      const circle = svg
-        .append('svg:g')
-        .selectAll('circle')
-        .data(force.nodes())
-        .enter()
-        .append('svg:circle')
-        .attr('r', 6);
+    const circle = svg
+      .append('svg:g')
+      .selectAll('circle')
+      .data(force.nodes())
+      .enter()
+      .append('svg:circle')
+      .attr('r', 6);
       // .call(force.drag);
 
-      const text = svg
-        .append('svg:g')
-        .selectAll('g')
-        .data(force.nodes())
-        .enter()
-        .append('svg:g');
+    const text = svg
+      .append('svg:g')
+      .selectAll('g')
+      .data(force.nodes())
+      .enter()
+      .append('svg:g');
 
-      // A copy of the text with a thick white stroke for legibility.
-      text
-        .append('svg:text')
-        .attr('x', 8)
-        .attr('y', '.31em')
-        .attr('class', 'shadow')
-        .text((d) => d.name.split('&')[0]);
+    // A copy of the text with a thick white stroke for legibility.
+    text
+      .append('svg:text')
+      .attr('x', 8)
+      .attr('y', '.31em')
+      .attr('class', 'shadow')
+      .text((d) => d.name.split('&')[0]);
 
-      text
-        .append('svg:text')
-        .attr('x', 8)
-        .attr('y', '.31em')
-        .text((d) => d.name.split('&')[0]);
+    text
+      .append('svg:text')
+      .attr('x', 8)
+      .attr('y', '.31em')
+      .text((d) => d.name.split('&')[0]);
 
-      // Use elliptical arc path segments to doubly-encode directionality.
-      function tick() {
-        path.attr('d', (d) => {
-          const dx = d.target.x - d.source.x;
-          const dy = d.target.y - d.source.y;
-          const dr = Math.sqrt(dx * dx + dy * dy);
-          return `M${d.source.x},${d.source.y}A${dr},${dr} 0 0,1 ${d.target.x},${d.target.y}`;
-        });
+    // Use elliptical arc path segments to doubly-encode directionality.
+    function tick() {
+      path.attr('d', (d) => {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const dr = Math.sqrt(dx * dx + dy * dy);
+        return `M${d.source.x},${d.source.y}A${dr},${dr} 0 0,1 ${d.target.x},${d.target.y}`;
+      });
 
-        circle.attr('transform', (d) => `translate(${d.x},${d.y})`);
-
-        text.attr('transform', (d) => `translate(${d.x},${d.y})`);
-      }
-    // });
+      circle.attr('transform', (d) => `translate(${d.x},${d.y})`);
+      text.attr('transform', (d) => `translate(${d.x},${d.y})`);
+    }
   }
 
   render() {
