3237002d7f67ce9ea266f620e052e5797e142045
Kevin Sun <kvnhsun@gmail.com>
2020-04-15 16:11:04 -0400

    refactored as functional component; need to fix css/sizing

--- client/components/QueryResult.js
+++ client/components/QueryResult.js
@@ -1,70 +1,35 @@
 import React, { Component } from 'react';
-import CodeMirror from 'codemirror';
+import { UnControlled as CM } from 'react-codemirror2';
 
 import '../styles/codemirror.css';
 
-require('codemirror/addon/hint/show-hint');
-require('codemirror/addon/comment/comment');
-require('codemirror/addon/edit/matchbrackets');
-require('codemirror/addon/edit/closebrackets');
 require('codemirror/addon/fold/foldgutter');
 require('codemirror/addon/fold/brace-fold');
-require('codemirror/addon/search/search');
-require('codemirror/addon/search/searchcursor');
-require('codemirror/addon/search/jump-to-line');
 require('codemirror/addon/dialog/dialog');
 require('codemirror/addon/lint/lint');
 require('codemirror/keymap/sublime');
-require('codemirror-graphql/hint');
-require('codemirror-graphql/lint');
-require('codemirror-graphql/info');
-require('codemirror-graphql/jump');
+
 require('codemirror-graphql/results/mode');
 
-import {UnControlled as CM} from 'react-codemirror2'
+function QueryResult(props) {
+  const options = {
+    lineWrapping: true,
+    readOnly: true,
+    tabSize: 2,
+    mode: 'graphql-results',
+    foldGutter: {
+      minFoldSize: 4,
+    },
+    lint: true,
+    gutters: ['CodeMirror-foldgutter'],
+  };
 
-class QueryResult extends Component {
-
-  
-  //   componentDidMount() {
-  //     this.editor = CodeMirror(document.getElementById('queryresult'), {
-  //       value: this.props.result || '',
-  //       lineWrapping: true,
-  //       readOnly: true,
-  //       tabSize: 2,
-  //       mode: 'graphql-results',
-  //       foldGutter: {
-  //         minFoldSize: 4,
-  //       },
-  //     });
-  //   }
-
-  //   componentDidUpdate(prevProps) {
-  //     if (this.props.results && this.props.result !== prevProps.result) {
-  //       this.editor.setValue(this.props.result);
-  //     }
-  //   }
-
-  render() {
-    const options = {
-      lineWrapping: true,
-      readOnly: true,
-      tabSize: 2,
-      mode: 'graphql-results',
-      foldGutter: {
-        minFoldSize: 4,
-      },
-      lint: true,
-      gutters: ['CodeMirror-foldgutter'],
-    };
-
-    return (
+  return (
       <CM
-        value={this.props.result || ''}
+        value={props.result || ''}
         options={options}
       />
-    );
-  }
+  );
 }
 
 export default QueryResult;
