9dfcd2d11e86e23e7dcedeffb8679bb72931d8c7
Meisze Yau <meiszey@gmail.com>
2020-04-16 17:05:05 -0400

    Fixed issue with shared field node

--- client/components/utilities/highlighterFunction.js
+++ client/components/utilities/highlighterFunction.js
@@ -5,6 +5,21 @@
 
 /* eslint-disable */
 export const highlightQuery = (schemaCopy, queryPath) => {
+  const duplicateFields = schemaCopy.duplicates;
+  for (let key in queryPath) {
+    for (let target in queryPath[key]) {
+      // Rewrite the target names for those that are duplicate fields 
+      // in the query path data to match what is in schema 
+      if (duplicateFields.includes(target.split('&')[0])) {
+        const newTargetName = target.split('&')[0] + '&multiple';
+        queryPath[key][newTargetName] = {
+          'duration': queryPath[key][target],
+          'parentPath': target.split('&')[1]
+        }
+        delete queryPath[key][target];
+      }
+    }
+  }
   if (schemaCopy.links.length) {
     for (let i = 0; i < schemaCopy.links.length; i++) {
       // check for matches with the keys in the query path
@@ -12,7 +27,6 @@ export const highlightQuery = (schemaCopy, queryPath) => {
       const pathTarget = schemaCopy.links[i].target.name
       const parentNodes = Object.keys(queryPath);
       // highlights all parent nodes if they are the source/target node
-      console.log('parent', parentNodes);
       if (parentNodes.includes(pathSource)) {
         schemaCopy.links[i].source.highlighted = true;
       }
@@ -26,6 +40,8 @@ export const highlightQuery = (schemaCopy, queryPath) => {
       }
       // Highlights the links between fields that point to other Type nodes
       const queryPathVals = Object.values(queryPath);
+      // contains an object with keys of all the target nodes for the paths 
+      // and values of each path's execution time 
       const queryPathValsObj = Object.assign(...queryPathVals);
       if (queryPathValsObj[pathSource]) {
         schemaCopy.links[i].source.highlighted = true;
@@ -33,6 +49,5 @@ export const highlightQuery = (schemaCopy, queryPath) => {
       }
     }
   }
-  console.log(schemaCopy);
   return schemaCopy;
 }
