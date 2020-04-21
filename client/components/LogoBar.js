bdf46774e9f24e4df1542f5e229363e365fe4db6
Taylor Quartararo <quartararo.t@gmail.com>
2020-04-21 15:04:15 -0400

    added icons

--- client/components/LogoBar.js
+++ client/components/LogoBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { GithubOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 function LogoBar(props) {
   return (
     <div id="logo-bar">
-        <img id='logo' src='../img/logosvg.svg'></img>
-        <GithubOutlined />
-        <GithubOutlined />
-        <GithubOutlined />
+        <img id='logo' src='../img/logopink.svg'></img>
+        <div id='icons'>
+            <Button type="link">About</Button>
+            <Button type="link">Docs</Button>
+            <a href='https://github.com/xerographica/gql-health-prototyper'><GithubOutlined /></a>
+        </div>
     </div>
   );
 }
