61857f01ae83861853be5a484fdb4c37a2a8b606
Kevin Sun <kvnhsun@gmail.com>
2020-04-25 15:59:28 -0400

    refactor w/ router v5; successfully sets isAuth state in App upon successful login

--- client/App.js
+++ client/App.js
@@ -1,18 +1,25 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import MainApp from './components/MainApp';
 import Register from './components/Register';
 import Login from './components/Login';
 
 const App = () => {
+  const [isAuthed, setIsAuthed] = useState(false);
+
+  const auth = () => {
+    setIsAuthed(true);
+  };
+  console.log(isAuthed);
+
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/" component={MainApp} />
+          <Route path="/register"> <Register/> </Route>
+          <Route path="/login"> <Login auth={auth}/> </Route>
+          <Route exact path="/"> <MainApp/> </Route>
         </Switch>
       </Router>
 
