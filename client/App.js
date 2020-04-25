014c8b8eb46373d6ac0c5ab64709e76ccb5dfe0b
Kevin Sun <kvnhsun@gmail.com>
2020-04-25 16:28:28 -0400

    renders homepage if user has not logged in or continued as guest

--- client/App.js
+++ client/App.js
@@ -4,22 +4,35 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import MainApp from './components/MainApp';
 import Register from './components/Register';
 import Login from './components/Login';
+import Homepage from './components/Homepage';
 
 const App = () => {
   const [isAuthed, setIsAuthed] = useState(false);
+  const [guest, setGuest] = useState(false);
 
   const auth = () => {
     setIsAuthed(true);
   };
-  console.log(isAuthed);
 
+  const continueGuest = () => {
+    setGuest(true);
+  };
+
+  console.log(isAuthed, guest)
+  console.log(isAuthed || guest)
   return (
+
     <div>
       <Router>
         <Switch>
           <Route path="/register"> <Register/> </Route>
           <Route path="/login"> <Login auth={auth}/> </Route>
-          <Route exact path="/"> <MainApp/> </Route>
+          <Route path="/">
+            {/* Render homepage if user has not logged in or continued as guest */}
+            {(isAuthed || guest)
+              ? <MainApp isAuthed={isAuthed}/>
+              : <Homepage continueGuest={continueGuest}/>}
+          </Route>
         </Switch>
       </Router>
 
