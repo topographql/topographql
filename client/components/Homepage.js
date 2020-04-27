afae5aea63c40afc654607b0738dd5cdc04a8bee
Taylor Quartararo <quartararo.t@gmail.com>
2020-04-27 18:34:44 -0400

    home page css

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Homepage = (props) => (
    <div id="homepage">
-    <h1>Welcome to TopoGraphQL!</h1> <br/>
-    <Link to="/login">Login</Link> <br/>
-    <Link to="/register">Register</Link> <br/>
-    <Link onClick={props.continueGuest} to="/">Or continue as a guest</Link>
+      <div>
+         <h1>Welcome to TopoGraphQL!</h1> <br/>
+         <Link to="/login">Login</Link> <br/>
+         <Link to="/register">Register</Link> <br/>
+         <Link onClick={props.continueGuest} to="/">Or continue as a guest</Link>
+      </div>
    </div>
 );
 
